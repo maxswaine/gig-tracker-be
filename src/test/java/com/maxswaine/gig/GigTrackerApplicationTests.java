6fc70ea0763bc7df876666f853ad6ce59df39421
maxswaine <108192224+maxswaine@users.noreply.github.com>
2024-04-18 15:59:09 +0100

    Initial Commit

---
+++ src/test/java/com/maxswaine/gig/GigTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maxswaine.gig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GigTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
