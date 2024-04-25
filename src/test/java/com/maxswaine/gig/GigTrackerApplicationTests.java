3880d5d8ec7424a710440ca7e5d5dc91970071e1
maxswaine <maxswaine@gmail.com>
2024-04-25 01:20:49 +0100

    formatting change

--- src/test/java/com/maxswaine/gig/GigTrackerApplicationTests.java
+++ src/test/java/com/maxswaine/gig/GigTrackerApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class GigTrackerApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
