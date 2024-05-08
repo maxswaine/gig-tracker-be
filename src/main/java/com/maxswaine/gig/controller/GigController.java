04d1eae6ade68b4ff09cc6e26e636f51615293b4
maxswaine <maxswaine@gmail.com>
2024-05-08 23:15:15 +0100

    Made adjustments to send response entity as well as actual json. Added better logging as well so errors are more easily spotted

--- src/main/java/com/maxswaine/gig/controller/GigController.java
+++ src/main/java/com/maxswaine/gig/controller/GigController.java
@@ -1,9 +1,11 @@
 package com.maxswaine.gig.controller;
 
 import com.maxswaine.gig.api.dto.Gig;
+import com.maxswaine.gig.api.dto.Moment;
 import com.maxswaine.gig.service.GigService;
-import lombok.extern.slf4j.Slf4j;
+import com.maxswaine.gig.service.MomentService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -11,52 +13,65 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/gig")
-@Slf4j
 public class GigController {
 
     private final GigService gigService;
+    private final MomentService momentService;
 
     @Autowired
-    public GigController(GigService gigService) {
+    public GigController(GigService gigService, MomentService momentService) {
         this.gigService = gigService;
+        this.momentService = momentService;
     }
 
     // READ
     @GetMapping
-    public List<Gig> getAllGigs() {
-        gigService.getAllGigs();
-        return gigService.getAllGigs().getBody();
-
+    public ResponseEntity<List<Gig>> getAllGigs() {
+        List<Gig> allGigs = gigService.getAllGigs();
+        return new ResponseEntity<>(allGigs, HttpStatus.OK);
     }
 
     @GetMapping(params = "artist")
-    public List<Gig> getGigByArtist(String artist) {
+    public ResponseEntity<List<Gig>> getGByArtist(String artist) {
         if (artist != null) {
             artist = artist.replace("_", " ");
         }
-        return gigService.getGigsByArtist(artist);
+        return new ResponseEntity<>(gigService.getGigsByArtist(artist), HttpStatus.OK);
     }
 
-    @GetMapping(params = "id")
-    public Gig getGigById(Long id){
-        return gigService.getGigbyId(id);
+    @GetMapping(params = "venue")
+    public ResponseEntity<List<Gig>> getGigsByVenue(String venue){
+        return new ResponseEntity<>(gigService.getGigsbyVenue(venue), HttpStatus.OK);
     }
 
     // CREATE
     @PostMapping
-    public void addGig(@RequestBody Gig gig) {
-        gigService.addGig(gig);
+    public ResponseEntity<Void> addGigWithMoments(@RequestBody Gig gig) {
+        Gig savedGig = gigService.addGig(gig);
+
+        if (savedGig != null && savedGig.getId() != null && !savedGig.getMoments().isEmpty()) {
+            for (Moment moment : savedGig.getMoments()) {
+                moment.setGig(savedGig);
+            }
+            momentService.addMoments(savedGig.getMoments());
+        }
+
+        return new ResponseEntity<>(HttpStatus.CREATED);
     }
+
+
 
     // DELETE
     @DeleteMapping(path = "{gigId}")
-    public void deleteGig(@PathVariable("gigId") Long id) {
+    public ResponseEntity<Void> deleteGig(@PathVariable("gigId") Long id) {
         gigService.deleteGig(id);
+        return new ResponseEntity<>(HttpStatus.ACCEPTED);
     }
 
     // UPDATE
     @PutMapping(path = "{gigId}")
-    public void updateGig(@PathVariable("gigId") Long id, @RequestBody Gig gig) {
-        gigService.updateGig(id, gig.getArtist(), gig.getVenue(), gig.getLocation(), gig.getDate(), gig.isFavourite());
+    public ResponseEntity<Gig> updateGig(@PathVariable("gigId") Long id, @RequestBody Gig gig) {
+        Gig updatedGig = gigService.updateGig(id, gig.getArtist(), gig.getVenue(), gig.getLocation(), gig.getDate(), gig.isFavourite());
+        return new ResponseEntity<>(updatedGig, HttpStatus.OK);
     }
 }
--- src/main/java/com/maxswaine/gig/service/GigService.java
+++ src/main/java/com/maxswaine/gig/service/GigService.java
@@ -2,14 +2,11 @@ package com.maxswaine.gig.service;
 
 import com.maxswaine.gig.api.dto.Gig;
 import com.maxswaine.gig.repository.GigRepository;
-import jakarta.persistence.EntityNotFoundException;
 import jakarta.transaction.Transactional;
 import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
@@ -19,9 +16,8 @@ import java.util.List;
 @Slf4j
 public class GigService {
 
-    private final GigRepository gigRepository;
-
     private static final Logger logger = LoggerFactory.getLogger(GigService.class);
+    private final GigRepository gigRepository;
 
 
     @Autowired
@@ -29,21 +25,25 @@ public class GigService {
         this.gigRepository = gigRepository;
     }
 
-    public ResponseEntity<List<Gig>> getAllGigs() {
-        log.info("Received request to get all gigs.");
-        return new ResponseEntity<>(gigRepository.findAll(), HttpStatus.OK);
+    public List<Gig> getAllGigs() {
+        logger.info("Fetching all gigs");
+        List<Gig> allGigs = gigRepository.findAll();
+        logger.info("Retrieved {} gigs", allGigs.size()); // Logging the number of gigs retrieved
+        return allGigs;
     }
 
-    public Gig getGigbyId(Long id) {
-        Gig gigById = gigRepository.findById(id)
-                .orElseThrow(() -> new EntityNotFoundException("Gig not found with that id " + id));
-        System.out.println(gigById);
-        return gigById;
+    public List<Gig> getGigsbyVenue(String venue) {
+        List<Gig> gigsByVenue = gigRepository.findGigsByVenueContainingIgnoreCase(venue);
+        if (gigsByVenue.isEmpty()) {
+            logger.warn("No gigs found with that venue");
+        }
+        logger.info("Found {} gigs at that venue", gigsByVenue.size());
+        return gigsByVenue;
     }
 
     public List<Gig> getGigsByArtist(String artist) {
         List<Gig> gigsByArtist = gigRepository.findGigsByArtistContainingIgnoreCase(artist);
-        if(gigsByArtist.isEmpty()){
+        if (gigsByArtist.isEmpty()) {
             logger.error("There are no artists that contain {}", artist);
         }
         System.out.println("Getting " + gigsByArtist.size() + " artists:\n");
@@ -53,38 +53,60 @@ public class GigService {
         return gigsByArtist;
     }
 
-    public void addGig(Gig gig) {
-        gigRepository.save(gig);
+    public Gig addGig(Gig gig) {
+        logger.info("Gig created:");
+        logger.info(gig.toString());
+        return gigRepository.save(gig);
     }
 
     public void deleteGig(Long id) {
         boolean exists = gigRepository.existsById(id);
         if (!exists) {
             logger.error("Gig does not exist with id: {}", id);
+        } else {
+            logger.info("Gig {} successfully deleted", id);
         }
         gigRepository.deleteById(id);
     }
 
     @Transactional
-    public void updateGig(Long id, String artist, String venue, String location, LocalDateTime date, boolean favourite) {
+    public Gig updateGig(Long id, String artist, String venue, String location, LocalDateTime date, boolean favourite) {
         Gig gig = gigRepository.getReferenceById(id);
+        int changes = 0;
+
         if (artist != null && !artist.isEmpty() && !gig.getArtist().equals(artist)) {
+            changes++;
+            logger.info("Artist changed to {}", artist);
             gig.setArtist(artist);
         }
         if (venue != null && !venue.isEmpty() && !gig.getVenue().equals(venue)) {
+            changes++;
+            logger.info("Venue changed to {}", venue);
             gig.setVenue(venue);
         }
         if (location != null && !location.isEmpty() && !gig.getLocation().equals(location)) {
+            changes++;
+            logger.info("Location changed to {}", location);
             gig.setLocation(location);
         }
         if (date != null && !gig.getDate().equals(date)) {
+            changes++;
+            logger.info("Date changed to {}", date);
             gig.setDate(date);
         }
         if (favourite != gig.isFavourite()) {
+            changes++;
+            if (favourite){
+                logger.info("This gig is now favourited");
+            } else{
+                logger.info("This gig has been unfavourited");
+            }
             gig.setFavourite(favourite);
         }
 
-        System.out.println("Updated Gig:\n" + gig);
+        logger.info("A total of {} changes were made to Gig with id {}", changes, id);
+
+        return gig;
 
     }
 
